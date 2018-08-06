5d4a268e2e4a48731dde9efd12a199011fe1920f
= <zachary31@outlook.com>
2018-08-06 16:32:07 +0800

    modify redis conf

--- ocr/src/main/java/com/chineseall/controller/TestController.java
+++ ocr/src/main/java/com/chineseall/controller/TestController.java
@@ -1,9 +1,12 @@
 package com.chineseall.controller;
 
 import com.chineseall.util.ResultUtil;
+import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.annotation.Resource;
 
 /**
  * @author gy1zc3@gmail.com
@@ -12,8 +15,23 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class TestController {
 
+    @Resource
+    private StringRedisTemplate template;
+
     @RequestMapping("/test")
     public ResponseEntity test() {
         return ResponseEntity.ok(ResultUtil.success("哈哈"));
+    }
+
+    @RequestMapping("/testRedis")
+    public ResponseEntity testRedis() {
+
+        if (!template.hasKey("shabao")) {
+            template.opsForValue().append("shabao", "jjjj");
+        } else {
+            template.delete("shabao");
+        }
+
+        return ResponseEntity.ok("hh");
     }
 }
